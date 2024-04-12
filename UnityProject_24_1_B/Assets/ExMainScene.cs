24949812d2b9e2cde44350292c934c1f965a44ec
parksarang04 <qkrtkfkd9074@naver.com>
2024-04-12 15:44:16 +0900

    [Feat] 씬 이동 까지 & 정리

---
+++ UnityProject_24_1_B/Assets/ExMainScene.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ExMainScene : MonoBehaviour
+{
+    public Text PointUI;
+    void Start()
+    {
+        PointUI.text = PlayerPrefs.GetInt("Point").ToString();
+    }
+
+
+    public void GoToGame()
+    {
+        SceneManager.LoadScene("GameScene_Shoot");
+    }
+}
